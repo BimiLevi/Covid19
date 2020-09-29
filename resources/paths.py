899de12eaba388192692001a4a23ae2bba66fe28
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-09-29 12:13:58 +0300

    new resources files

--- resources/paths.py
+++ resources/paths.py
@@ -1,26 +1,28 @@
 import os
-
 ''' In order to use selenium, you need to download webdriver from: https://www.selenium.dev/downloads/,
  after downloading the webdriver add the path in this script.
  '''
 
 webDriver_path = 'C:\Web_drivers\Chromewebdriver\chromedriver.exe'
+site_url = "https://www.worldometers.info/coronavirus"
 
-current_dir = os.getcwd()
 
-utilities_path = current_dir + r'\utilities'
-world_path = current_dir + r'\World Meter Data'
-countriesID_path = utilities_path + r'\countries id'
-continentsID_path = utilities_path + r'\continent id'
-mainCountries_path = current_dir + r'\main csvs\main_countries.csv'
-mainContinents_path = current_dir + r'\main csvs\main_continents.csv'
+project_path = os.path.dirname(os.path.dirname(__file__))
+# project_path = os.path.join(project_root, 'Covid19')
+
+world_path = os.path.join(project_path, 'World Meter Data')
+utilities_path = os.path.join(project_path, 'utilities')
+resources_path = os.path.join(project_path, 'resources')
+
+countries_path = os.path.join(resources_path, 'Countries table')
+continents_path = os.path.join(resources_path, 'Continents table')
+countries_id = os.path.join(resources_path, 'Countries id')
 
 def creat_paths():
-	from process_func import get_date_parm
+	from scraper.process_func import get_date_parm
 	day, month, year = get_date_parm()
 
 	# Crating directories to save the scraped data in them.
-	from paths import current_dir
 	#  Crating path for each directory.
 	dateYea_path = world_path + '/' + year
 	dateMon_path = dateYea_path + '/' + month
---
+++ resources/tables_func.py
@@ -0,0 +1,15 @@
+from resources.paths import countries_path, continents_path
+from db.engine import engine
+import pandas as pd
+
+def continents_table():
+	continents = pd.read_json(continents_path + '.json', orient = 'index').reset_index()
+	continents = continents.rename(columns = {'index': 'Continent', 0: 'Continents_id'})
+	continents = continents.reindex(columns = ['Continents_id', 'Continent'])
+	continents.to_sql('Continents', con = engine, index = False)
+	print('Continents table has been successfully created.\n')
+
+def countries_table():
+	countries = pd.read_json(countries_path + '.json', orient = 'columns')
+	countries.to_sql('Countries', con = engine, index = False)
+	print('Countries table has been successfully created.\n')

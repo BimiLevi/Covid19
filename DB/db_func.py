c5f4cfcf680ec6f8f271c1e106014ef3ae577afc
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-09-29 12:13:07 +0300

    Update

--- DB/db_func.py
+++ DB/db_func.py
@@ -1,80 +1,67 @@
 from db.engine import engine
 
-def country_data_toDB(countries_df):
+def load_backup():
+	import time
+	import os
+	from glob import glob
+	import pandas as pd
+	from resources.paths import world_path
 	try:
-		countries_list = countries_df['Country'].drop_duplicates().tolist()
-		for country in countries_list:
-			countryDF = countries_df[countries_df['Country'] == '{}'.format(country)]
-			countryDF.to_sql('{}'.format(country), con = engine, if_exists = 'append', index = False)
-		print('Countries DB was successfully Updated.')
-
-	except ConnectionError as e:
-		print('An error has occurred when trying to update countries DB.')
-		raise e
-
-def continent_data_toDB(continent_df):
-	try:
-		continent_list = continent_df['Continent'].drop_duplicates().tolist()
-		for continent in continent_list:
-			continentDF = continent_df[continent_df['Continent'] == '{}'.format(continent)]
-			continentDF.to_sql('{}'.format(continent), con = engine, if_exists = 'append', index = False)
-		print('Continents DB was successfully Updated.')
-
-	except ConnectionError as e:
-		print('An error has occurred when trying to update continents DB.')
-		raise e
-
-def load_backup_toDB():
-	try:
-		import time
 		start = time.time()
 
-		import pandas as pd
-		from paths import mainCountries_path, mainContinents_path
-		countries = pd.read_csv(mainCountries_path)
-		continents = pd.read_csv(mainContinents_path)
+		# Complexity time O(n^3)
+		ext_list = ["*Countries*", '*Continents*']
+		for ext in ext_list:
+			all_csv_files = []
+			for path, subdir, files in os.walk(world_path):
+				for csv_path in glob(os.path.join(path, ext)):
+					df = pd.read_csv(csv_path, index_col = None, header = 0)
+					all_csv_files.append(df)
 
-	except FileNotFoundError as e:
-		raise e
+			frame = pd.concat(all_csv_files, axis = 0, ignore_index = True)
+			frame = frame.sort_values('scrap_date')
 
-	try:
-		country_data_toDB(countries)
-		continent_data_toDB(continents)
+			if ext == "*Countries*":
+				df_to_db('Country', frame)
+
+			elif ext == '*Continents*':
+				df_to_db('Continent', frame)
 
 		end = time.time()
 		execution_time = (end - start) / 60
 		print('The process executed successfully,the time it took is: {:.3f} minutes.'.format(execution_time))
 
-	except KeyError as e:
+	except Exception as e:
+		print('The following Exception as occurred:\n{}'.format(e))
+
+def get_all_tables():
+	tables_list = []
+	with engine.connect() as con:
+		res = con.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+		tables = res.fetchall()
+		for table in tables:
+			tables_list.append(table[0])
+
+	return tables_list
+
+def table_exists(table):
+	tables_list = get_all_tables()
+
+	if table in tables_list:
+		return True
+
+	else:
+		return False
+
+def df_to_db(col, df):
+	try:
+		headers_list = df[col].drop_duplicates().tolist()
+		for header in headers_list:
+			temp_df = df[df[col] == '{}'.format(header)]
+			temp_df.to_sql('{}'.format(header), con = engine, if_exists = 'append', index = False)
+
+		print('{} DB was successfully Updated.'.format(col))
+
+	except ConnectionError as e:
+		print('An error has occurred when trying to update continents DB.')
 		raise e
-
-def load_backup():
-	import os
-	from glob import glob
-	import pandas as pd
-	from paths import world_path
-
-	# Complexity time O(n^3)
-	ext_list = ["*Countries*", '*Continents*']
-	for ext in ext_list:
-		all_csv_files = []
-		for path, subdir, files in os.walk(world_path):
-			for csv_path in glob(os.path.join(path, ext)):
-				df = pd.read_csv(csv_path, index_col = None, header = 0)
-				all_csv_files.append(df)
-
-		frame = pd.concat(all_csv_files, axis = 0, ignore_index = True)
-		frame = frame.sort_values('scrap_date')
-
-		if ext == '*Countries*':
-			country_data_toDB(frame)
-
-		elif ext == '*Continents*':
-			continent_data_toDB(frame)
-
-
-
-
-if __name__ == '__main__':
-
-	pass

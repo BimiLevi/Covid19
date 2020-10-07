0cf5a8814522cac06bc4b0d2eb5b2df45974d23c
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-10-07 15:21:28 +0300

    Now using windows scheduler

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from scraper.world_meter_scraper import get_data, data_to_dfs, data_to_csvs
 from db.db_func import load_backup, df_to_db, table_exists
 from resources.tables_func import *
 import time
-import schedule
 
 def main():
     start = time.time()
@@ -13,12 +12,18 @@ def main():
             print('Creating Countries main table')
             countries_table()
 
+    except Exception as e:
+        print("Couldn't create Countries base table.")
+        print("The error that occurred is:\n{}".format(e))
+
+    try:
+
         if not table_exists('Continents'):
             print('Creating Continents main table')
             continents_table()
 
     except Exception as e:
-        print('Cannot creat the base tables.')
+        print('Cannot creat the Continents base table.')
         print("The error that occurred is:\n{}".format(e))
 
     try:
@@ -64,12 +69,14 @@ def main():
 
 
 if __name__ == '__main__':
-    load_backup()
+    main()
 
-    schedule.every().day.at("22:00").do(main)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(60)  # Wait one minute
+    """If you would like to use schedule model you can use the code below."""
+    import schedule
+    # schedule.every().day.at("22:00").do(main)
+    #
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(60)  # Wait one minute
 
 

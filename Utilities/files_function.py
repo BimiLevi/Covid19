e3dc777a66dd0d70d6bdfb189145a03654e074f1
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-10-12 13:33:28 +0300

    Update

--- Utilities/files_function.py
+++ Utilities/files_function.py
@@ -1,6 +1,6 @@
 import json
 
-def json_save(data, file_name, file_type = 'json'):
+def save_json(data, file_name, file_type = 'json'):
 	try:
 		with open(file_name+'.{}'.format(file_type), 'w') as outfile:
 			json.dump(data, outfile)

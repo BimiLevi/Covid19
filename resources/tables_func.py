9d69cdec857244726e8b9cc246d7c5a2e5d244e8
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-10-14 14:04:45 +0300

    Update
    Signed-off-by: BimiLevi <50989568+BimiLevi@users.noreply.github.com>

--- resources/tables_func.py
+++ resources/tables_func.py
@@ -1,9 +1,9 @@
-from resources.paths import countries_path, continents_path
-from db.engine import engine
 import pandas as pd
 
+from resources.paths import countries_path, continents_path
 
-def continents_table():
+
+def continents_table(engine):
 	try:
 		continents = pd.read_json(continents_path + '.json', orient = 'index').reset_index()
 		continents = continents.rename(columns = {'index': 'Continent', 0: 'Continent_id'})
@@ -14,7 +14,7 @@ def continents_table():
 	except Exception as e:
 		print('The following Exception as occurred:\n{}'.format(e))
 
-def countries_table():
+def countries_table(engine):
 	try:
 		countries = pd.read_json(countries_path + '.json', orient = 'columns')
 		countries.to_sql('Countries', con = engine, index = False)

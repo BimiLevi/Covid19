647b52f5268b6cd832b02969a23923405cd41114
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-10-27 19:32:32 +0200

    func added
    Signed-off-by: BimiLevi <50989568+BimiLevi@users.noreply.github.com>

--- analysis/analysis_func.py
+++ analysis/analysis_func.py
@@ -41,3 +41,42 @@ def get_minmax(df, col):
 
 	min_max = pd.concat([min_val, max_val])
 	return min_max
+
+def get_top(df, col, n=10):
+
+	if (type(n) != int) or (type(col) != str):
+		raise TypeError('n = int, col = str')
+
+	elif not isinstance(df, pd.DataFrame):
+		raise TypeError('df = dataframe')
+
+
+	col_list = df.columns.tolist()
+
+	if 'Continent' in col_list:
+		if col == 'TotalTests':
+			return
+
+		sorted_df = df[['Continent', col]].sort_values(col, ascending = False)
+		top = sorted_df.head(n).reset_index(drop = True)
+		return top
+
+	elif 'Country' in col_list:
+		sorted_df = df[['Country', col]]	.sort_values(col, ascending = False)
+		top = sorted_df.head(n).reset_index(drop = True)
+		return top
+
+	else:
+		return 'Error neither countries or continents were called.'
+
+def get_table_list(names):
+	df_list = []
+
+	for name in names:
+		df = db.get_table(name)
+		df_list.append(df)
+
+	return df_list
+
+
+

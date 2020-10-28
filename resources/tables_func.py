7e6486625759e4afb654411d7de3597198dd012d
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-10-28 12:48:09 +0200

    dtype parm added
    Signed-off-by: BimiLevi <50989568+BimiLevi@users.noreply.github.com>

--- resources/tables_func.py
+++ resources/tables_func.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+from database.tables_parm import *
 from resources.paths import countries_path, continents_path
 
 
@@ -8,7 +9,7 @@ def continents_table(engine):
 		continents = pd.read_json(continents_path + '.json', orient = 'index').reset_index()
 		continents = continents.rename(columns = {'index': 'Continent', 0: 'Continent_id'})
 		continents = continents.reindex(columns = ['Continent_id', 'Continent'])
-		continents.to_sql('All Continents', con = engine, index = False)
+		continents.to_sql('All Continents', con = engine, index = False, dtype=continents_parm)
 		print('Continents table has been successfully created.\n')
 
 	except Exception as e:
@@ -17,7 +18,7 @@ def continents_table(engine):
 def countries_table(engine):
 	try:
 		countries = pd.read_json(countries_path + '.json', orient = 'columns')
-		countries.to_sql('All Countries', con = engine, index = False)
+		countries.to_sql('All Countries', con = engine, index = False, dtype=countries_parm)
 		print('Countries table has been successfully created.\n')
 
 	except Exception as e:

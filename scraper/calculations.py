d16818c57e1e9caf793cd1337598d43221c97e3c
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-10-29 12:58:34 +0200

    first
    Signed-off-by: BimiLevi <50989568+BimiLevi@users.noreply.github.com>

---
+++ scraper/calculations.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+from database.db_config import current_db as db
+
+
+def get_last_row(tableName):
+	row = db.get_table(tableName).tail(1)
+	return row
+
+def calc_new_cases(df):
+	series = None
+	try:
+		dif = df['TotalCases'].diff()
+		df['NewCases'] = dif
+		series = df['NewCases'].iloc[1]
+
+	except Exception as e:
+		print(e)
+
+	finally:
+		return series
+
+def calc_new_deaths(df):
+	series = None
+	try:
+		dif = df['TotalDeaths'].diff()
+		df['NewDeaths'] = dif
+		series = df['NewDeaths'].iloc[1]
+
+	except Exception as e:
+		print(e)
+
+	finally:
+		return series
+
+def calc_new_recovered(df):
+	series = None
+	try:
+		dif = df['TotalRecovered'].diff()
+		df['NewRecovered'] = dif
+		series = df['NewRecovered'].iloc[1]
+
+	except Exception as e:
+		print(e)
+
+	finally:
+		return series
+
+def make_calc(df, tableName):
+	# try:
+		pd.options.mode.chained_assignment = None  # default='warn'
+
+		last_row = get_last_row(tableName)
+
+		tempdf = pd.concat([last_row, df]).reset_index(drop=True)
+
+		for col in ['TotalCases','NewCases','TotalDeaths','NewDeaths','TotalRecovered','NewRecovered']:
+			tempdf[col] = tempdf[col].astype('float64')
+
+		df['NewCases'] = calc_new_cases(tempdf)
+		df['NewDeaths'] = calc_new_deaths(tempdf)
+		df['NewRecovered'] = calc_new_recovered(tempdf)
+
+		return df
+
+	# except Exception as e:
+	# 	print(e)

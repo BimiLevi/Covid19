8f8d7062c098c7a494d604290c60d0ed975ce5b1
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-11-12 16:36:06 +0200

    country cod added
    Signed-off-by: BimiLevi <50989568+BimiLevi@users.noreply.github.com>

--- analysis/analysis_func.py
+++ analysis/analysis_func.py
@@ -2,91 +2,108 @@ import calendar
 
 import numpy as np
 import pandas as pd
+import pycountry
 from geopy.geocoders import Nominatim
 from pycountry_convert import country_alpha2_to_continent_code, country_name_to_country_alpha2
 
 
 def data_by_month(df, month, year):
-    if type(month) != int:
-        raise TypeError('Month must be of int type.')
+	if type(month) != int:
+		raise TypeError('Month must be of int type.')
 
-    data = df[(df['scrap_date'].dt.month == month) & (df['scrap_date'].dt.year == year)]
-    return data.reset_index(drop = True)
+	data = df[(df['scrap_date'].dt.month == month) & (df['scrap_date'].dt.year == year)]
+	return data.reset_index(drop = True)
+
 
 def data_range_date(df, startDate, endDate):
-    firstDate = df['scrap_date'].min().date()
-    lastDate = df['scrap_date'].max().date()
+	firstDate = df['scrap_date'].min().date()
+	lastDate = df['scrap_date'].max().date()
 
-    startDate = pd.to_datetime(startDate).date()
-    endDate = pd.to_datetime(endDate).date()
+	startDate = pd.to_datetime(startDate).date()
+	endDate = pd.to_datetime(endDate).date()
 
-    if (startDate < firstDate) or (endDate > lastDate) or (startDate > endDate):
-        raise ValueError('One of the dates surpass boundaries')
+	if (startDate < firstDate) or (endDate > lastDate) or (startDate > endDate):
+		raise ValueError('One of the dates surpass boundaries')
 
-    data = df[(str(startDate) <= df['scrap_date']) & (df['scrap_date'] <= str(endDate))].reset_index(drop = True)
-    return data
+	data = df[(str(startDate) <= df['scrap_date']) & (df['scrap_date'] <= str(endDate))].reset_index(drop = True)
+	return data
+
 
 def get_minmax(df, col):
-    min_val = df[df[col] == df[col].min()][col]
-    max_val = df[df[col] == df[col].max()][col]
+	min_val = df[df[col] == df[col].min()][col]
+	max_val = df[df[col] == df[col].max()][col]
 
-    min_max = pd.concat([min_val, max_val])
-    return min_max
-
-def get_top(df, col, n=10):
-
-    if (type(n) != int) or (type(col) != str):
-        raise TypeError('n = int, col = str')
-
-    elif not isinstance(df, pd.DataFrame):
-        raise TypeError('df = dataframe')
+	min_max = pd.concat([min_val, max_val])
+	return min_max
 
 
-    col_list = df.columns.tolist()
+def get_top(df, col, n = 10):
+	if (type(n) != int) or (type(col) != str):
+		raise TypeError('n = int, col = str')
 
-    if 'Continent' in col_list:
-        if col == 'TotalTests':
-            return
+	elif not isinstance(df, pd.DataFrame):
+		raise TypeError('df = dataframe')
 
-        sorted_df = df[['Continent', col]].sort_values(col, ascending = False)
-        top = sorted_df.head(n).reset_index(drop = True)
-        return top
+	col_list = df.columns.tolist()
 
-    elif 'Country' in col_list:
-        sorted_df = df[['Country', col]]	.sort_values(col, ascending = False)
-        top = sorted_df.head(n).reset_index(drop = True)
-        return top
+	if 'Continent' in col_list:
+		if col == 'TotalTests':
+			return
 
-    else:
-        return 'Error neither countries or continents were called.'
+		sorted_df = df[['Continent', col]].sort_values(col, ascending = False)
+		top = sorted_df.head(n).reset_index(drop = True)
+		return top
+
+	elif 'Country' in col_list:
+		sorted_df = df[['Country', col]].sort_values(col, ascending = False)
+		top = sorted_df.head(n).reset_index(drop = True)
+		return top
+
+	else:
+		return 'Error neither countries or continents were called.'
+
 
 def first_day_of_month(date):
-    day = calendar.weekday(date.year, date.month, 1)
-    return calendar.day_name[day]
+	day = calendar.weekday(date.year, date.month, 1)
+	return calendar.day_name[day]
+
 
 def get_codes(col):
-    try:
-        cn_a2_code = country_name_to_country_alpha2(col)
-    except:
-        cn_a2_code = 'Unknown'
-    try:
-        cn_continent = country_alpha2_to_continent_code(cn_a2_code)
-    except:
-        cn_continent = 'Unknown'
-    return (cn_a2_code, cn_continent)
+	try:
+		cn_a2_code = country_name_to_country_alpha2(col)
+	except:
+		cn_a2_code = 'Unknown'
+	try:
+		cn_continent = country_alpha2_to_continent_code(cn_a2_code)
+	except:
+		cn_continent = 'Unknown'
+	return (cn_a2_code, cn_continent)
 
-geolocator = Nominatim(user_agent="https")
+
+geolocator = Nominatim(user_agent = "https")
+
 def geolocate(country):
-    loc = None
+	loc = None
 
-    try:
-        loc = geolocator.geocode(country)
-        loc = (loc.latitude, loc.longitude)
+	try:
+		loc = geolocator.geocode(country)
+		loc = (loc.latitude, loc.longitude)
 
-    except:
-        loc = np.nan
+	except:
+		loc = np.nan
 
-    finally:
-        return loc
+	finally:
+		return loc
 
 
+def country_code(col):
+	cod = []
+	for country in col:
+		try:
+			code = pycountry.countries.get(name = country).alpha_3
+			cod.append(code)
+
+		except:
+			cod.append('None')
+
+	return cod

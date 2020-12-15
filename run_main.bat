7cecfc458786d0585c3e2f5058442f7b707db1f4
BimiLevi <50989568+BimiLevi@users.noreply.github.com>
2020-12-15 15:12:13 +0200

    update
    Signed-off-by: BimiLevi <50989568+BimiLevi@users.noreply.github.com>

--- run_main.bat
+++ run_main.bat
@@ -1,1 +1,1 @@
-"C:\Users\talle\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\talle\PycharmProjects\Covid19\main.py"
+"C:\Users\talle\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\talle\PycharmProjects\Covid19\main.py"
